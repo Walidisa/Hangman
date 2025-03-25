913bfaeaf1aa78436eecf880c599941de75b7999
Isa <walidisa976@gmail.com>
2025-03-25 21:23:08 +0300

    first commit

---
+++ Hangman/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            hangmanPic = new PictureBox();
+            startButton = new Button();
+            richTextBox1 = new RichTextBox();
+            richTextBox2 = new RichTextBox();
+            guessButton = new Button();
+            inputText = new RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)hangmanPic).BeginInit();
+            SuspendLayout();
+            // 
+            // hangmanPic
+            // 
+            hangmanPic.BackgroundImage = (Image)resources.GetObject("hangmanPic.BackgroundImage");
+            hangmanPic.Image = (Image)resources.GetObject("hangmanPic.Image");
+            hangmanPic.Location = new Point(26, 12);
+            hangmanPic.Name = "hangmanPic";
+            hangmanPic.Size = new Size(313, 396);
+            hangmanPic.TabIndex = 0;
+            hangmanPic.TabStop = false;
+            hangmanPic.Click += hangmanPic_Click;
+            // 
+            // startButton
+            // 
+            startButton.BackColor = Color.Green;
+            startButton.Font = new Font("Segoe UI", 20F);
+            startButton.Location = new Point(374, 12);
+            startButton.Name = "startButton";
+            startButton.Size = new Size(183, 108);
+            startButton.TabIndex = 1;
+            startButton.Text = "Start";
+            startButton.UseVisualStyleBackColor = false;
+            startButton.Click += startButton_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BackColor = SystemColors.Window;
+            richTextBox1.BorderStyle = BorderStyle.None;
+            richTextBox1.Font = new Font("Segoe UI", 20F);
+            richTextBox1.Location = new Point(374, 143);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.RightToLeft = RightToLeft.No;
+            richTextBox1.Size = new Size(391, 106);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Enabled = false;
+            richTextBox2.Font = new Font("Segoe UI", 10.1F, FontStyle.Bold);
+            richTextBox2.Location = new Point(373, 286);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(230, 85);
+            richTextBox2.TabIndex = 3;
+            richTextBox2.Text = "Enter a letter and Click Guess ";
+            // 
+            // guessButton
+            // 
+            guessButton.BackColor = Color.SkyBlue;
+            guessButton.Font = new Font("Segoe UI", 16F);
+            guessButton.Location = new Point(614, 12);
+            guessButton.Name = "guessButton";
+            guessButton.Size = new Size(151, 108);
+            guessButton.TabIndex = 5;
+            guessButton.Text = "Guess";
+            guessButton.UseVisualStyleBackColor = false;
+            guessButton.Click += guessButton_Click;
+            // 
+            // inputText
+            // 
+            inputText.AccessibleName = "guessButton";
+            inputText.AccessibleRole = AccessibleRole.PushButton;
+            inputText.Enabled = false;
+            inputText.Font = new Font("Segoe UI", 20F);
+            inputText.Location = new Point(668, 291);
+            inputText.Name = "inputText";
+            inputText.Size = new Size(97, 80);
+            inputText.TabIndex = 6;
+            inputText.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(800, 460);
+            Controls.Add(inputText);
+            Controls.Add(guessButton);
+            Controls.Add(richTextBox2);
+            Controls.Add(richTextBox1);
+            Controls.Add(startButton);
+            Controls.Add(hangmanPic);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "Hangman";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)hangmanPic).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox hangmanPic;
+        private Button startButton;
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private Button guessButton;
+        private RichTextBox inputText;
+    }
+}
---
+++ Hangman/Form1.cs
@@ -0,0 +1,180 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace Hangman
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        string[] words = {
+            "apple",
+            "banana",
+            "cat",
+            "dog",
+            "car",  
+            "bus",
+            "bike",
+            "cherry",
+            "house",
+            "elephant",
+            "giraffe",
+            "hippo",
+            "iguana",
+            "jaguar",
+            "leopard"
+         };
+        int index;
+        string word = "";
+        string text = "";
+        int lose = 0;
+        int count = 0;
+        Boolean win = false;
+        string basePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "hangmanPics");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hangmanPic_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            index = rand.Next(words.Length);
+            word = words[index];
+            guessButton.Enabled = true;
+            inputText.Enabled = true;
+            richTextBox1.BackColor = Color.White;
+            richTextBox1.Enabled = false;
+            richTextBox1.Text = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                richTextBox1.Text += "_ ";
+                richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+                startButton.Enabled = false;
+            }
+            
+
+        }
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+           
+            Boolean check = false;
+            int charIndex = 0;
+            if (inputText.Text.Length > 0)
+            {
+               char a = inputText.Text[0];
+               for (int i = 0; i<word.Length; i++)
+                {
+                    if (a == word[i])
+                    {
+                        charIndex = i;
+                        check = true;
+                    }
+                }
+                if (check)
+                {
+                    text = "";
+                    for (int i = 0; i < richTextBox1.Text.Length; i++)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            if (word[i/2] == a)
+                            {
+                                text += a + " ";
+                                richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+                            }
+
+                            else
+                            {
+                                text += richTextBox1.Text[i] + " ";
+                                richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+                                startButton.Enabled = false;
+                            }
+                        }
+
+                    }
+                    for (int i = 0; i < richTextBox1.Text.Length; i++)
+                    {
+                        if (richTextBox1.Text[i] == '_' || richTextBox1.Text[i] == ' ')
+                        {
+                            win = false;
+                        }
+                        else
+                        {
+                            count++;
+                        }
+                    }
+                    if (count == word.Length)
+                    {
+                        win = true;
+                        richTextBox1.Enabled = true;
+                        richTextBox1.BackColor = Color.Green;
+                    }
+                     richTextBox1.Text = text;
+                     richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+                }
+                else
+                {
+                    MessageBox.Show("You guessed the wrong letter!");
+                    lose++;
+                    switch (lose)
+                    {
+                        case 1:
+                            hangmanPic.Image = Image.FromFile(Path.Combine(basePath, "head.png"));
+                            break;
+                        case 2:
+                            hangmanPic.Image = Image.FromFile(Path.Combine(basePath, "head+body.png"));
+                            break;
+                        case 3:
+                            hangmanPic.Image = Image.FromFile(Path.Combine(basePath, "head+body+rightarm.png"));
+                            break;
+                        case 4:
+                            hangmanPic.Image = Image.FromFile(Path.Combine(basePath, "head+body+allArms.png"));
+                            break;
+                        case 5:
+                            hangmanPic.Image = Image.FromFile(Path.Combine(basePath, "head+body+allArms+rightLeg.png"));
+                            break;
+                        case 6:
+                            hangmanPic.Image = Image.FromFile(Path.Combine(basePath, "fullsize.png"));
+                            richTextBox1.Enabled = true;
+                            richTextBox1.BackColor = Color.Red;
+                            MessageBox.Show("Oops the Hangman has Died! You couldn't Guess the Correct Word! \nPress Start to start another game");
+                            hangmanPic.Image = Image.FromFile(Path.Combine(basePath, "empty.png"));
+                            startButton.Enabled = true;
+                            guessButton.Enabled = false;
+                            inputText.Enabled = false;
+                            lose = 0;
+                            break;
+
+                    }
+                }
+                inputText.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Enter a letter.");
+            }
+            if (win)
+            {
+                MessageBox.Show("Congratulations! You have guessed the correct word and Saved the Hangman! \nPress Start to generate another word and start another game");
+
+                startButton.Enabled = true;
+                guessButton.Enabled = false;
+                inputText.Enabled = false;
+                hangmanPic.Image = Image.FromFile("C:/Users/walid/Downloads/empty.png");
+                lose = 0;
+                count = 0;
+                win = false;
+            }
+        }
+    }
+}
